11ab3109df8985486c47f5bc5119239b2ce7affc
Mikhail Smagin <mvsmagin@mail.ru>
2022-10-07 16:09:41 +0300

    task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -37,7 +37,7 @@ function addInCartCountChange(value) {
     // присвойте ей значение oldValue
     newValue = oldValue;
     // увеличьте значение newValue на 1
-    newValue = oldValue + '1';
+    newValue = oldValue + 1;
     // Конец решения задания №1.2.
 
     return newValue;
